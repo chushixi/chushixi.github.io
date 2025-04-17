5adf2cdf7f3f3bf99439e4417c9d773f4d1bd2fe
simako <chushixi@gmail.com>
2025-04-17 22:33:52 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
-let currentPage = 0;
-let totalPages = document.querySelectorAll('.page').length;
+let currentPage = 1;
+const slider = document.querySelector('.simako-exe');
+const totalPages = document.querySelectorAll('.page').length;
 
 function slideTo(index) {
-  const slider = document.querySelector('.simako-exe');
   slider.style.transform = `translateX(-${index * 100}vw)`;
 }
 
 function goRight() {
   currentPage += 1;
-  if (currentPage === totalPages) {
-    currentPage = 0;
-  }
   slideTo(currentPage);
+  
+  if (currentPage === totalPages - 1) {
+    setTimeout(() => {
+      slider.style.transition = 'none';
+      currentPage = 1;
+      slideTo(currentPage);
+      setTimeout(() => {
+        slider.style.transition = 'transform 0.5s ease';
+      }, 50);
+    }, 500);
+  }
 }
 
 function goLeft() {
   currentPage -= 1;
-  if (currentPage < 0) {
-    currentPage = totalPages - 1;
-  }
   slideTo(currentPage);
+  
+  if (currentPage === 0) {
+    setTimeout(() => {
+      slider.style.transition = 'none';
+      currentPage = totalPages - 2;
+      slideTo(currentPage);
+      setTimeout(() => {
+        slider.style.transition = 'transform 0.5s ease';
+      }, 50);
+    }, 500);
+  }
 }
